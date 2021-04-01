4640f80a177dc8f29326dc08716ac8709b2cf813
hans0 <kiessler.hans@gmail.com>
2021-04-01 18:25:12 -0400

    Added Nezumi Graverobber to testData

--- src/App.js
+++ src/App.js
@@ -4,34 +4,27 @@ import './App.css';
 import Card from './components/Card';
 import DFCard from './components/DFCard';
 
+import TestCardDisplay from './components/TestCardDisplay';
+
 import { cards } from './testData';
 
 function App() {
 
-  console.log(cards);
+  // console.log(cards);
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* Manually showing card display */}
+      
+      
       <Card card={cards[0]}/>
       <Card card={cards[1]}/>
       <DFCard card={cards[2]}/>
       <DFCard card={cards[3]}/>
       <DFCard card={cards[4]}/>
-      <Card card={cards[0]}/>
+     
+
+      {/* <TestCardDisplay /> */}
 
     </div>
   );
--- src/components/TestCardDisplay.js
+++ src/components/TestCardDisplay.js
@@ -1,14 +1,26 @@
+import Card from './Card';
 import DFCard from './DFCard';
-import { cards } from './testData';
 
-const TestCardDisplay = (props) => {
+import { cards } from '../testData';
+
+import '../App.css';
+
+
+const TestCardDisplay = () => {
   console.log(cards);
   return(
-    <>
-    {props.cards.map((c) => 
-      {c.dfname ? <DFCard card={c} /> : <Card card={c} /> }
-    )}
-    </>
+    <div>
+      {cards.map((card, index) => 
+        card.dfname ? <DFCard key={index} card={card} /> : <Card key={index} card={card} />
+        // Boolean(c.dfname) ? <DFCard card={c} /> : <Card card={c} />
+        // console.log(Boolean(c.dfname));
+      
+        // {
+        //   !c.dfname ? <DFCard card={c} /> : <Card card={c} /> 
+        // }
+      )}
+      
+    </div>
   );
 }
 
